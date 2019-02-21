9c6409a700639020fb63a384429c330e72b743ce
Raul Anaya Rojo <raulmartin_04@hotmail.com>
2019-02-20 21:14:51 -0700

    Create data-types.dart

---
+++ data-types.dart
@@ -0,0 +1,12 @@
+void main() {
+  
+  int intNumber = 12;
+  String strName = 'Raul';
+  double dblNumber = 15.83;
+  bool bolBolean = true;
+  
+  print(intNumber);
+  print(strName);
+  print(dblNumber);
+  print(bolBolean);
+}

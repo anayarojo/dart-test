575e3229a694dc1e54083d63a45abfb8be2b6b6b
anayarojo <raulmartin_04@hotmail.com>
2020-03-22 12:29:27 -0700

    Create 04_comments dart file

---
+++ exercises/01_basic/04_comments.dart
@@ -0,0 +1,43 @@
+/**
+ * This file is for explain how use comments
+ * [CreatedAt]: 22/03/2020
+ */
+
+
+main() {
+
+  // Esta es una variable temporal que quiero definir
+  int a = 10; // A debería de ser 10
+
+  /*
+  Este es un comentario multilinea
+  final people = [
+    // Esta es la primera persona
+    'Raul', // [0]
+
+    // Esta es la segunda persona
+    'Martin', // [1]
+
+    // Esta es la tercera persona
+    'Jose', // [2]
+
+    // Esta es la cuarta persona
+    'Manuel', // [3]
+
+    // Esta es la quinta persona
+    'Ana', // [4]
+
+    // Esta es la quinta persona
+    'Alejandra' // [5]
+  ];
+
+  print(people);
+  */
+
+  greet(4, 9);
+    
+}
+
+/// Function to greet
+/// Receives a [name] and concat it with the [message].
+greet(int name, int message) {}

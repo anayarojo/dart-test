6b325025825c8bc04d5d2f3d98a9b4eac55cf5f1
anayarojo <raulmartin_04@hotmail.com>
2020-03-22 13:40:09 -0700

    Create 04_for_in_loop dart file

---
+++ exercises/02_control/04_for_in_loop.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+main() {
+
+  List<String> characters = ['Kakashi', 'Sazuke', 'Naruto', 'Sakura'];
+
+  for(String character in characters) {
+    stdout.writeln(character);
+  }
+
+}
